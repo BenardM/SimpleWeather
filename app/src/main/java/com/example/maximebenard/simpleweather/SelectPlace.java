b44363ca54628f4d98618ea35fee025c9db24f75
BenardM <maxime.benardb2b@gmail.com>
2017-07-05 08:39:59 +0200

    Modified activity_main.xml -> TextViews

---
+++ app/src/main/java/com/example/maximebenard/simpleweather/SelectPlace.java
@@ -0,0 +1,19 @@
+package com.example.maximebenard.simpleweather;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+
+public class SelectPlace extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_place);
+        
+    }
+
+}
